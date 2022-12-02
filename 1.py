20460f70874387b4b3b50314ae0e42e1895a42d2
kevinlinxc <44324811+kevinlinxc@users.noreply.github.com>
2022-12-01 18:39:30 -0800

    Clean up day 1

--- 1.py
+++ 1.py
@@ -1,31 +1,29 @@
 import fileinput
 
 lines = [line.strip() for line in fileinput.input(files="inputs/1.txt")]
-print(lines)
 
+# part 1
 max_cal = 0
 current = 0
-
-for line in lines:
-    # todo don't ignore last line
-    if line == '':
+for index, line in enumerate(lines):
+    if line == '' or index == len(lines)-1:
         max_cal = max(current, max_cal)
         current = 0
     else:
         current += int(line)
 
-
 print(max_cal)
 
+
+# part 2
 counts = []
 current = 0
-for line in lines:
-    if line == '':
+for index, line in enumerate(lines):
+    if line == '' or index == len(lines)-1:
         counts.append(current)
         current = 0
     else:
         current += int(line)
 counts.append(current)
 
-print(sorted(counts, reverse=True))
 print(sum(sorted(counts, reverse=True)[0:3]))
